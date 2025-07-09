3b6b63c2f09781ff197182d657f6e4f3ea2b5542
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-07-09 15:29:43 +0200

    Add security backend config

---
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -0,0 +1,42 @@
+package com.company.enroller.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableConfigurationProperties(JwtProperties.class)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final ParticipantProvider participantProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProperties jwtProperties;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), jwtProperties), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtProperties.getSecret()))
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/participants").permitAll()
+                .antMatchers("/api/login").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
+    }
+}
