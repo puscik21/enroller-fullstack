d4f6afd143e49f7f71f2818acf6cd37e5c2ffc14
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-07-12 13:19:42 +0200

    Add 'register' option

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,15 +3,21 @@ import "milligram"
 import styled from "styled-components";
 import UserPanel from "./user/UserPanel";
 import LoginForm from "./user/LoginForm";
-import {loginRequest} from "./api/authApi";
+import {loginRequest, registerRequest} from "./api/authApi";
 
 function App() {
     const [loggedInUser, setLoggedInUser] = useState("");
 
     // TODO: what will happen when JWT is expired
-    const onLogin = async (login) => {
-        if (await loginRequest(login, "password")) {
+    const onLogin = async (login, password) => {
+        if (await loginRequest(login, password)) {
             setLoggedInUser(localStorage.getItem("loggedInUser"));
+        }
+    }
+
+    const onRegister = async (login, password) => {
+        if (await registerRequest(login, password)) {
+            console.log("Yupiiii !!!") // TODO: use toasts
         }
     }
 
@@ -24,7 +30,8 @@ function App() {
     return (
         <Container>
             <h1>Meetings enroller system</h1>
-            {isLoggedIn ? <UserPanel login={loggedInUser} onLogout={onLogout}/> : <LoginForm onLogin={onLogin}/>}
+            {isLoggedIn ? <UserPanel login={loggedInUser} onLogout={onLogout}/> :
+                <LoginForm onLogin={onLogin} onRegister={onRegister}/>}
         </Container>
     );
 }
--- src/main/frontend/src/user/LoginForm.js
+++ src/main/frontend/src/user/LoginForm.js
@@ -1,15 +1,66 @@
 import {useState} from "react";
+import styled from "styled-components";
 
-const LoginForm = ({onLogin}) => {
+const LoginForm = ({onLogin, onRegister}) => {
     const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = () => {
+        if (areCredentialsFilled()) onLogin(login, password);
+    };
+
+    const handleRegister = () => {
+        if (areCredentialsFilled()) onRegister(login, password);
+    };
+
+    const areCredentialsFilled = () => {
+        if (!login || !password) {
+            setError("Fill credentials");
+            return false;
+        }
+        setError("");
+        return true;
+    }
 
     return (
-        <div>
-            <label>Login by e-mail:</label>
-            <input type="text" value={login} onChange={e => setLogin(e.target.value)}/>
-            <button onClick={() => onLogin(login)}>Login</button>
-        </div>
+        <Container>
+            {error && <ErrorLabel aria-live={"polite"}>{error}</ErrorLabel>}
+            <label>Login</label>
+            <input
+                type="text"
+                value={login}
+                onChange={e => setLogin(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <button type="button" onClick={handleLogin}>Login</button>
+            <button type="button" className="button button-outline" onClick={handleRegister}>Register</button>
+        </Container>
     )
 }
 
+const ErrorLabel = styled.span`
+    color: #af1313;
+    font-size: 2rem;
+`
+
 export default LoginForm;
+
+const Container = styled.div`
+    width: 50%;
+    margin: 50px auto;
+    padding: 32px;
+    border-radius: 16px;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.08);
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    min-width: 320px;
+    max-width: 500px;
+`;
